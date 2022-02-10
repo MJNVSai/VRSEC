30d7a74a6139163df94536bb27d8168ae80a221b
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-10 20:38:07 +0530

    Create merged.c

---
+++ Data-structures/week-9/merged.c
@@ -0,0 +1,16 @@
+/*
+A polynomial p(x) is the expression in variable x which is in the form (axn + bxn-1 + …. + jx+ k), where a, b, c …., k fall in the category of real numbers and 'n' is non negative integer.
+which is called the degree of polynomial. Create a function that takes two sorted linked lists and merges them into a single sorted linked list. Your goal is to return a new linked list that contains the nodes from two lists in sorted order.
+You may assume the sort order is ascending.
+
+For example:
+// list1
+1 -> 4 -> 10 -> 11
+
+// list2
+-1 -> 2 -> 3 -> 6
+
+// merged list
+-1 -> 1 -> 2 -> 3 -> 4 -> 6 -> 10 -> 11
+
+*/
