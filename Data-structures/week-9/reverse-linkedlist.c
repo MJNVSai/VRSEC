6bbd25be0c27f3a93ba54e5597cdede42525f5c9
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-10 20:39:30 +0530

    Create reverse-linkedlist.c

---
+++ Data-structures/week-9/reverse-linkedlist.c
@@ -0,0 +1,10 @@
+/*
+Reverse a linked list: Given pointer to the head node of a linked list, the task is to reverse the linked list. 
+We need to reverse the list by changing the links between nodes.
+
+Input: Head of following linked list 
+1->2->3->4->NULL 
+Output: Linked list should be changed to, 
+4->3->2->1->NULL
+
+*/
