3dd8f9874d5623ae4b94b8ef765848d031a2003a
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-11 21:26:21 +0530

    Update begin-insert.c

--- Data-structures/week-10/begin-insert.c
+++ Data-structures/week-10/begin-insert.c
@@ -15,3 +15,84 @@ Use the following pseudocode. Justify your solution with all the possible test c
 7.	head=new;
 
 */
+
+// Program:
+
+#include <stdio.h>
+#include <stdlib.h>
+// task 2
+int flag,e;
+ 
+struct node{
+    struct node*prev;
+    int data;
+    struct node*next;
+}*new,*head,*temp,*helper;
+ 
+// double linked list
+ 
+void dll()
+{
+
+    do
+    {
+        printf("Enter an element : ");
+        scanf("%d",&e);
+        new = (struct node*)malloc(sizeof(struct node*));
+        new->prev = NULL;
+        new->data = e;
+        new->next = NULL;
+        if(head == NULL)
+        {
+            head = new;
+            temp = new;
+        }
+        else
+        {
+            new->next = head;
+            head->prev = new;
+            head = head->prev;
+        }
+        printf("Do you want to enter an other node (1/0) : ");
+        scanf("%d",&flag);
+    
+    }while(flag==1);
+}
+
+void display()
+{
+    helper = head;
+    printf("NULL ");
+    
+    while(helper!=NULL)
+    {
+        printf("%d ",helper->data);
+        helper = helper->next;
+    }
+    printf("NULL");
+}
+ 
+int main()
+{
+    dll();
+    display();
+ 
+    return 0;
+}
+
+/*
+OUTPUT
+
+Enter an element : 10
+Do you want to enter an other node (1/0) : 1
+Enter an element : 20
+Do you want to enter an other node (1/0) : 1
+Enter an element : 30
+Do you want to enter an other node (1/0) : 1
+Enter an element : 40
+Do you want to enter an other node (1/0) : 1
+Enter an element : 50
+Do you want to enter an other node (1/0) : 0
+NULL 50 40 30 20 10 NULL
+
+*/
