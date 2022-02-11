846964dac8448d1f268aafa808b8d29eec9ea292
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-11 21:14:01 +0530

    Create double-linkedlist.c

---
+++ Data-structures/week-10/double-linkedlist.c
@@ -0,0 +1,37 @@
+/*
+Design and Implement a program to create double linked list. Use the following pseudocode.
+Justify your solution with all the possible test cases. 
+
+// Pseudocode:
+// create node
+1.	Struct node
+2.	{
+3.	Struct node *prev;
+4.	Int data;
+5.	Struct nodel * next;
+6.	} *new,*temp,*head;
+// create double linked list
+1.	do
+2.	{
+3.	….
+4.	….
+5.	….
+6.	….
+7.	….
+8.	….
+9.	If(head=NULL) 
+10.	{
+11.	Head=new;
+12.	Temp=new;
+13.	}
+14.	else
+15.	{
+16.	….
+17.	….
+18.	….
+19.	}
+20.	Printf(“do you need one more term”);
+21.	Scanf(“%c”,&ch);
+22.	}while(ch==’y’);
+
+*/
