2655928d252cf25ea7563a0e210757f53cd54baa
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-11 21:15:46 +0530

    Create begin-insert.c

---
+++ Data-structures/week-10/begin-insert.c
@@ -0,0 +1,17 @@
+// Insert a Node at begining of the Double linked List
+
+/*
+Design and Implement a program to insert node in the beginning of the double linked list.
+Use the following pseudocode. Justify your solution with all the possible test cases.
+(Use the code designed in task-1 and make extension).
+
+// Insert at begin
+1.	new=…………………..   //allocate memory
+2.	new-->prev=NULL;
+3.	…….
+4.	……..                    // read value
+5.	Newdata=value;
+6.	Newnext=head;
+7.	head=new;
+
+*/
