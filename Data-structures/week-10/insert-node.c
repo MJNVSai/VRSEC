2bd5cdd93a91dce0128317b85fb7d8e832768736
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-02-11 21:17:16 +0530

    Create insert-node.c

---
+++ Data-structures/week-10/insert-node.c
@@ -0,0 +1,6 @@
+/*
+
+Based on the knowledge acquired from Task-1 and Task-2. Perform (i) Insert of node at the end of a double linked list.
+and (ii) Insert of node at the specified position of a double linked list.
+
+*/
